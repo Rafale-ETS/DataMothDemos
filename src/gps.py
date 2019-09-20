6e79dad6adc7315476833aafd55a5571cfba657c
Speedy321 <christo-chibougamo@hotmail.com>
2019-09-20 15:23:01 -0400

    first gps data

--- src/gps.py
+++ src/gps.py
@@ -8,8 +8,11 @@ serCon = serial.Serial(serPort, 4800, timeout=5)
 
 while 1:
     line = serCon.readline()
-    print(line)
-    #splitline = line.split(',')
+    splitline = line.decode().split(',')
 
-    if line[0] == '$GPGGA':
-        print(line)
+    if splitline[0] == '$GPGGA':
+        latt = line[2]
+        latDirec = line[3]
+        longit = line[4]
+        longDirec = line[5]
+        print("lattitude: "+str(latt)+" lattDirect: "+str(latDirec)+" longitude: "+str(longit)+" longDirect: "+str(longDirec))
