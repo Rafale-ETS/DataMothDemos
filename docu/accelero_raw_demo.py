ba156b4d05db655ca8f34c6851a26a5521d2c8e9
Speedy321 <christo-chibougamo@hotmail.com>
2019-10-03 14:33:22 -0400

    fixed tabs not being spaces

--- docu/accelero_raw_demo.py
+++ docu/accelero_raw_demo.py
@@ -5,12 +5,11 @@ from mpu6050 import mpu6050
 
 accelero = mpu6050(0x68)
 
-
 while(1):
-	accel_data = accelero.get_accel_data()
+    accel_data = accelero.get_accel_data()
     temp = accelero.get_temp()
     gyro_data = accelero.get_gyro_data()
     #data = accelero.get_all_data()
-	print("accel: "+accel_data)
+    print("accel: "+accel_data)
     print(" temp: "+temp)
     print(" gyro: "+gyro_data)
