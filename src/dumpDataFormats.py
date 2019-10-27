10ff55d39b8ffd38359b4d39e464594fba801248
Christophe-A Gassmann <christo-chibougamo@hotmail.com>
2019-10-26 23:39:32 -0400

    Made dumpData to be able to populate the DB with dummy data

--- src/dumpDataFormats.py
+++ src/dumpDataFormats.py
@@ -1,12 +1,16 @@
 
 import json
 
-import timeUtil
+import timeUtil, time
+from database import mothDBSingle as mothDB
 
 #dump data formats:
 from gps import DataGGA, DataRMC, DataGPS
 from gyro_accel import DataAccel, DataGyro, DataTemperature
 from anemo import DataWind
+
+fillDB = True
+dumpJson = False
 
 class Data:
     def __init__(self, gps, acc, gyr, tem, win):
@@ -28,6 +32,29 @@ win = DataWind(timeUtil.getTimeNowAsNMEA(), timeUtil.getDateNowAsNMEA(), 6, 25)
 
 data = Data(gps.__dict__, acc.__dict__, gyr.__dict__, tem.__dict__, win.__dict__)
 
-f = open('docu/dataFormat.json', 'w+')
-f.write(json.dumps(data.__dict__, indent=4))
-f.close
+if dumpJson:
+    f = open('docu/dataFormat.json', 'w+')
+    f.write(json.dumps(data.__dict__, indent=4))
+    f.close
+
+if fillDB:
+    dataCount = 0
+    while(dataCount < 120): #devrais tourner pour 1min -ish a 2 points de donne /min
+        gps.time = timeUtil.getTimeNowAsNMEA()
+        gps.date = timeUtil.getDateNowAsNMEA()
+        acc.time = timeUtil.getTimeNowAsNMEA()
+        acc.date = timeUtil.getDateNowAsNMEA()
+        gyr.time = timeUtil.getTimeNowAsNMEA()
+        gyr.date = timeUtil.getDateNowAsNMEA()
+        tem.time = timeUtil.getTimeNowAsNMEA()
+        tem.date = timeUtil.getDateNowAsNMEA()
+        win.time = timeUtil.getTimeNowAsNMEA()
+        win.date = timeUtil.getDateNowAsNMEA()
+
+        mothDB.addGPSData(gps)
+        mothDB.addACCData(acc)
+        mothDB.addGYRData(gyr)
+        mothDB.addTEMData(tem)
+        mothDB.addWINData(win)
+
+        dataCount += 1
